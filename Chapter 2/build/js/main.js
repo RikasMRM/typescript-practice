f099cedd71cdeeb047938fb9dc49db207397f54a
Rikas <rikasrkf@gmail.com>
2023-07-06 13:38:27 +0530

    Chapter 2

---
+++ Chapter 2/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "Rikas";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Mohomed";
+meaningOfLife = 25;
+isLoading = true;
+album = 5150;
+const sunm = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ Chapter 2/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Rikas";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Mohomed";
+meaningOfLife = 25;
+isLoading = true;
+album = 5150;
+
+const sunm = (a: number, b: number) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
